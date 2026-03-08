966ce1bbe72dc53f803c3516a921d8694cc84ab9
KHong <albtrossfacefall@gmail.com>
2026-03-08 18:32:24 -0400

    begin sketching data display

--- apps/web/src/features/carousel/component/Carousel.jsx
+++ apps/web/src/features/carousel/component/Carousel.jsx
@@ -1,13 +1,23 @@
 'use client';
 import { useCallback, useEffect, useState } from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
+
+import { useClosetItemsQuery } from '../../closet/hooks/useClosetItemsQuery.js' 
 import styles from './Carousel.module.scss';
 
-export default function Carousel({}) {
+// PROPS should have a data member with an array of closetitems. e.g. {
+//      data: [ {ClosetItem}, {ClosetItem} ]
+// }
+export default function Carousel(props) {
+
+    // destructure data from props
+    const { data } = useClosetItemsQuery("hat");
+    console.log(data);
     const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false });
     const prevSlide = () => emblaApi?.scrollPrev();
     const nextSlide = () => emblaApi?.scrollNext();
 
+    // should be - for each item in data, create thumbnail with image/placeholder and link to closetitem
     return (
         <div className="embla">
             <div className={styles.embla__viewport} ref={emblaRef}>
