3d212fecc3fa448a498e42997c1bc571fee44850
KHong <albtrossfacefall@gmail.com>
2026-03-09 04:19:24 -0400

    make frontend-carousel

--- apps/web/src/app/(protected)/outfit-generator/page.js
+++ apps/web/src/app/(protected)/outfit-generator/page.js
@@ -1,11 +1,31 @@
+'use client';
+import { useState, useEffect } from 'react';
+
 import Carousel from '../../../features/carousel/component/Carousel.jsx';
+import { useClosetItemsQuery } from '../../../features/closet/hooks/useClosetItemsQuery.js';
+
 
 export default function OutfitGeneratorPage() {
-  return (
+  const [categories, setCategories] = useState(["hats"]);
+  const [closetData, setClosetData] = useState({});
+  
+  const {data: closetItems } = useClosetItemsQuery("hats");
+    // have async data be populated to an Object where key is the category name?
+
+  const removalCallback = (categoryName) => { setCategories(categories.filter(a => a != categoryName)); }
+
+    return (
     <main style={{ padding: 24 }}>
       <h1>Make an Outfit</h1>
       <p>Also protected ✅</p>
-      <Carousel />
+      { closetItems ? (
+          categories.map((categoryName, i) => {
+            return (
+              <Carousel key={i} data={closetItems} categoryName={categoryName} removalCallback={() => removalCallback(categoryName)}/>
+            );
+          }) 
+        ) : (<></>)
+      }
     </main>
   );
 }
--- apps/web/src/features/carousel/component/Carousel.jsx
+++ apps/web/src/features/carousel/component/Carousel.jsx
@@ -1,35 +1,75 @@
-'use client';
-import { useCallback, useEffect, useState } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
+import ClassNames from 'embla-carousel-class-names';
+import Link from 'next/link';
 
 import { useClosetItemsQuery } from '../../closet/hooks/useClosetItemsQuery.js' 
 import styles from './Carousel.module.scss';
+import './CarouselCardVisibility.scss';
 
 // PROPS should have a data member with an array of closetitems. e.g. {
 //      data: [ {ClosetItem}, {ClosetItem} ]
 // }
-export default function Carousel(props) {
-
-    // destructure data from props
-    const { data } = useClosetItemsQuery("hat");
+export default function Carousel({ data, categoryName, removalCallback }) {
     console.log(data);
-    const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false });
-    const prevSlide = () => emblaApi?.scrollPrev();
-    const nextSlide = () => emblaApi?.scrollNext();
+    const [selectedSnap, setSelectedSnap] = useState(0)
+    const [emblaRef, emblaApi] = useEmblaCarousel({ 
+        loop: true,
+        align: 'center'
+    }, [ClassNames()]);
+    
+    const remove = useCallback(() => {removalCallback()}, [removalCallback]);
+    const onSelect = (emblaApi, event) => { setSelectedSnap(emblaApi.selectedScrollSnap); };
 
-    // should be - for each item in data, create thumbnail with image/placeholder and link to closetitem
+    useEffect(() => {
+        if (!emblaApi) return;
+        emblaApi.on('select', onSelect);
+        return () => { emblaApi.off('select', onSelect); };
+    }, [emblaApi, selectedSnap]);
+
+    const determineSlideInnerClassName = i => {
+        let innerClassName = "carousel-slide-inner"
+        
+        if ((selectedSnap == 0 && i == data.length - 1) || (i == selectedSnap - 1)) {
+            innerClassName = innerClassName.concat(" view-left");
+        } else if ((selectedSnap == data.length - 1 && i == 0) || (i == selectedSnap + 1)) {
+            innerClassName = innerClassName.concat(" view-right");
+        }
+        return innerClassName;
+    };
+    
     return (
-        <div className="embla">
+        <div className="carousel">
+            <div className={styles.carousel_header}>
+                <h4>{categoryName}</h4>
+                
+                <button onClick={remove}>
+                    <span className="material-symbols-outlined" aria-hidden="true">
+                        remove
+                    </span>
+                </button>
+            </div>
             <div className={styles.embla__viewport} ref={emblaRef}>
                 <div className={styles.embla__container}>
-                    <div className={styles.embla__slide}>s1</div>
-                    <div className={styles.embla__slide}>s2</div>
-                    <div className={styles.embla__slide}>s3</div>
-                    <div className={styles.embla__slide}>s4</div>
+                    {   data ? 
+                        data.map((closetItem, i) => {
+                            return (
+                                <div className={styles.embla__slide} key={closetItem.id}>
+                                    <Link className={determineSlideInnerClassName(i)} href={`/closet-items/${closetItem.id}`}>
+                                        {
+                                            !closetItem.image ? 
+                                            (<span>{closetItem.name}</span>) 
+                                            : 
+                                            (<img href={closetItem.image} alt={closetItem.name}/>)
+                                        }
+                                    </Link>
+                                </div>
+                            )
+                        }) : (<></>)
+                    }
                 </div>
             </div>
-            <button className="embla__prev" onClick={prevSlide}>prev</button>
-            <button className="embla__next" onClick={nextSlide}>next</button>
         </div>
     );
 }
+
